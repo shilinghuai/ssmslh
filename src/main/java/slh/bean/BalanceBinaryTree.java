abf145d1fb5783811202ec5310a1c8b3fd4f441d
shilh <shilh@xingyuanauto.com>
2018-11-07 14:15:59 +0800

    synTest

---
+++ src/main/java/slh/bean/BalanceBinaryTree.java
@@ -0,0 +1,14 @@
+package slh.bean;
+/**
+* @Description:    二叉排序树
+* @Author:         ShiLinghuai
+* @CreateDate:     2018/11/7 10:50
+* @UpdateUser:     ShiLinghuai
+* @UpdateDate:     2018/11/7 10:50
+* @UpdateRemark:   修改内容
+* @Version:        1.0
+*/
+public class BalanceBinaryTree {
+  //节点的左孩子存储的数据小于父节点，节点的右孩子存储的数据大于父节点。
+
+}
